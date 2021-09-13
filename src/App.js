06912895060017d0df38ee69e87b53ebb86080b7
Kumar Divyanshu <divyanshu9707@gmail.com>
2021-09-13 17:31:19 +0530

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -33,7 +33,6 @@ class App extends React.Component{
               ...snapshot.data()
             }
           });
-          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth })
